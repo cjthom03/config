81d66b85791ee5f42369cb8a9342f6cfb267b361
Charlie Thomas <charlie.thomas@livelyme.com>
2023-08-05 19:19:51 -0700

    start collecting all the new config

---
+++ README.md
@@ -0,0 +1,2 @@
+# Configuration Backup
+Because I have destroyed my machine one too many times and maybe I have actually learned something?
---
+++ collect-config.zsh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+# get all the values from collect-config.zsh and print them to one line, separated by spaces
+# Path: .dotfiles/config/collect-config.zsh
